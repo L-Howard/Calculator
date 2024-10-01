e26da374f62a0434852f6e5c31e5bde1aaf571bf
Luke Howard <810363519+L-Howard@users.noreply.github.com>
2024-10-01 13:00:44 -0700

    Calculator layout in html and css completed Basic calculator functions completed

---
+++ javascript.js
@@ -0,0 +1,36 @@
+const add = function(a, b) {
+    return (a + b)
+};
+
+const subtract = function(a, b) {
+	return (a - b)
+};
+
+const multiply = function(a, b) {
+      return (a * b)
+};
+
+const divide = function (a, b) {
+    return (a / b)
+};
+
+let num1 = 0
+let operator
+let num2 = 0
+
+const operate = function (operator, a, b) {
+    if (operator === "+") {
+        return add(a, b)
+    }
+    else if (operator === "-") {
+        return subtract(a, b)
+    }
+    else if (operator === "*") {
+        return multiply(a, b)
+    }
+    else if (operator === "/") {
+        return divide(a, b)
+    }
+
+}
+
