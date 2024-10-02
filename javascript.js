c92a1313efc985c3528f7e1acb5a208948ebfa5d
Luke Howard <810363519+L-Howard@users.noreply.github.com>
2024-10-02 11:53:27 -0700

    Clicking buttons adds the text to the display.

--- javascript.js
+++ javascript.js
@@ -14,23 +14,44 @@ const divide = function (a, b) {
     return (a / b)
 };
 
-let num1 = 0
-let operator
-let num2 = 0
+let num1 = 0;
+let operator;
+let num2 = 0;
 
 const operate = function (operator, a, b) {
     if (operator === "+") {
         return add(a, b)
     }
+
     else if (operator === "-") {
         return subtract(a, b)
     }
+
     else if (operator === "*") {
         return multiply(a, b)
     }
+
     else if (operator === "/") {
         return divide(a, b)
     }
 
+}
+
+let displayValue = document.querySelector(".display")
+
+const buttons = document.querySelectorAll('button');
+
+    buttons.forEach(function(button) {
+        button.addEventListener("click", function(){
+            populateDisplay(button);
+        })
+    })
+
+function populateDisplay(button) {
+    console.log(button.innerText)
+    displayValue.textContent += button.innerText;
+
+
+    
 }
 
