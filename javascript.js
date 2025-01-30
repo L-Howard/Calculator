eeca91a277497450e1de112f65fdcb3379457539
Luke Howard <810363519+L-Howard@users.noreply.github.com>
2025-01-29 18:55:22 -0700

    New template for javascript

--- javascript.js
+++ javascript.js
@@ -1,77 +1,14 @@
-const add = function(a, b) {
-    return (a + b)
-};
-
-const subtract = function(a, b) {
-	return (a - b)
-};
-
-const multiply = function(a, b) {
-      return (a * b)
-};
-
-const divide = function (a, b) {
-    return (a / b)
-};
-
-let num1 = 0;
-let operator;
-let num2 = 0;
-
-const operate = function (operator, a, b) {
-    if (operator === "+") {
-        return add(a, b)
-    }
-
-    else if (operator === "-") {
-        return subtract(a, b)
-    }
-
-    else if (operator === "*") {
-        return multiply(a, b)
-    }
-
-    else if (operator === "/") {
-        return divide(a, b)
-    }
-
-}
-
-let displayValue = document.querySelector(".display")
+let operator = '';
+let previousValue = '';
+let currentValue = '';
 
 
-const buttons = document.querySelectorAll('button');
+document.addEventListener("DOMContentLoaded", function() {
+    let clear = document.querySelector(".clear");
+    let equal = document.querySelector(".equal");
+    let decimal = document.querySelector(".decimal");
 
-    buttons.forEach(function(button) {
-        button.addEventListener("click", function(){
-            populateDisplay(button);
-        })
-    })
-
-function populateDisplay(button) {
-    const value = button.innerText
-    if (value != "AC" && value != "<") {
-    displayValue.textContent += button.innerText;
-    }
-
-    else if (value === "AC") {
-        displayValue.textContent = ""
-        num1 = null;
-        operator = null;
-    }
-
-    else if (value === "<") {
-        displayValue.textContent = displayValue.textContent.slice(0, -1);
-    }
-
-    else if (value === "+") {
-        num1 = parseFloat(displayValue.textContent);
-        operator = "+"
-        displayValue.textContent = ""
-        console.log(num1)
-    }
-    
-
-}
-
+    let numbers = document.querySelectorAll(".number");
+    let operators = document.querySelectorAll(".opperator");
+}) 
 
